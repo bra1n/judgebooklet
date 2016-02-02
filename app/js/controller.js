463381ae16f6e515865e505e9191c72c49315c6b
Steffen <steffen@baumgart.biz>
2016-02-02 17:46:23 +0100

    shortcuts and penalties done

--- app/js/controller.js
+++ app/js/controller.js
@@ -4,7 +4,7 @@
     '$scope', '$translate', function($scope, $translate) {
       $scope.pageNumbers = [0, 1, 2, 3, 4, 5, 6, 7];
       $scope.pageList = ["turn", "layers", "abilities", "shortcuts", "penalties", "jar", "information", "announcements", "gpe", "competitive", "competitive2", "sealed", "2hg", "commander", "reviews", "cheating"];
-      $scope.pages = ["turn", "layers", "abilities"];
+      $scope.pages = ["turn", "layers", "abilities", "shortcuts", "penalties"];
       $scope.lastDrag = false;
       $scope.language = $translate.use();
       $scope.languages = ["fr", "de", "es", "it", "en"];
